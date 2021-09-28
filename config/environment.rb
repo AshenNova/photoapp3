435648ec98c64d954bb578972be381e77f422ee7
ashennova <kennerve14@gmail.com>
2021-09-28 16:19:29 +0800

    test7

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => Rails.application.credentials.SENDGRID_USERNAME,
-    :password       => Rails.application.credentials.SENDGRID_PASSWORD,
+    :user_name      => Rails.application.credentials[:sendgrid][:user_name],
+    :password       => Rails.application.credentials[:sendgrid][:password],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
 }
