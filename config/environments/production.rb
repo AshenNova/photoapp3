49bad8eb00bc7f5a4a3a6428e90a4adc85876fe7
ashennova <kennerve14@gmail.com>
2021-09-28 14:50:26 +0800

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,14 +9,14 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :domain => 'yourdomain.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
   }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default.url_options = { :host => 'ashennova-photoapp3.herokuapp.com/', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'ashennova-photoapp3.herokuapp.com', protocol => 'https'}
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
