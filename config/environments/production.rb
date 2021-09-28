0a1c47df46171069ed741315cc02dfc2d5abaa80
ashennova <kennerve14@gmail.com>
2021-09-28 14:03:12 +0800

    Setup email functionality for sign-up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,18 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config_action_mail.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  config_action_mailer.delivery_method = :smtp
+  config_action_mailer.default.url_options = { :host => 'ashennova-photoapp3.herokuapp.com/', :protocol => 'https' }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
