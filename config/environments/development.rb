4e9dab4787af14ae4a7eae6d00fa42f1913dbdd5
ashennova <kennerve14@gmail.com>
2021-09-28 23:02:25 +0800

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,18 +11,17 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-
-  # ActionMailer::Base.smtp_settings = {
-  #   :address => 'smtp.sendgrid.net',
-  #   :port => '587',
-  #   :authentication => :plain,
-  #   :user_name => Rails.application.credentials[:sendgrid][:user_name],
-  #   :password => Rails.application.credentials[:sendgrid][:password],
-  #   :domain => 'heroku.com',
-  #   :enable_starttls_auto => true
-  # }
-  # config.action_mailer.delivery_method :test
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => Rails.application.credentials[:sendgrid][:user_name],
+    :password => Rails.application.credentials[:sendgrid][:password],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method :test
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -87,3 +86,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+
+
