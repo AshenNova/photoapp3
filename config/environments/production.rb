0296feed40df305331404a598f94420a57b8a97a
ashennova <kennerve14@gmail.com>
2021-09-29 00:35:21 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,19 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+ config.action_mailer.default_url_options = { host: 'ashennova-photoapp3.herkuapp.com'}
+ config.action_mailer.delivery_method = :smtp
+ config.action.mailer.perform_deliveries = true
+ config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => Rails.application.credentials[:sendgrid][:user_name],
+    :password       => Rails.application.credentials[:sendgrid][:password],
+    :domain         => 'ashennova-photoapp3.herkuapp.com',
+    :enable_starttls_auto => true
+}
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
